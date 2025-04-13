152bec2103ac0c12a9f1e64aab79a8d78abb27d2
Nighty Death <1536434622@qq.com>
2025-04-13 17:06:51 +0800

    修改并添加部分内容

---
+++ src/main/java/com/nightydead/lordofmysteries/LordOfMysteriesClinet.java
@@ -0,0 +1,10 @@
+package com.nightydead.lordofmysteries;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LordOfMysteriesClinet implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
