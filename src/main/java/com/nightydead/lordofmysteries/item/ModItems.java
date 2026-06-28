dce518a51f4d7e1ac8ef7080a415fab7aa215c79
shangwu <1536434622@qq.com>
2026-06-28 14:56:55 +0800

    增加非凡特性分类

--- src/main/java/com/nightydead/lordofmysteries/item/ModItems.java
+++ src/main/java/com/nightydead/lordofmysteries/item/ModItems.java
@@ -12,23 +12,23 @@ public class ModItems {
     //非凡特性
     //region 占卜家途径
     public static final DeferredItem<Item> SEER_CHARACTERISTIC =
-            ITEMS.register("seer_characteristic", () -> new Item(new Item.Properties().fireResistant()));
+            ITEMS.register("characteristic/seer_characteristic", () -> new Item(new Item.Properties().fireResistant()));
     public static final DeferredItem<Item> CLOWN_CHARACTERISTIC =
-            ITEMS.register("clown_characteristic", () -> new Item(new Item.Properties().fireResistant()));
+            ITEMS.register("characteristic/clown_characteristic", () -> new Item(new Item.Properties().fireResistant()));
     public static final DeferredItem<Item> MAGICIAN_CHARACTERISTIC =
-            ITEMS.register("magician_characteristic", () -> new Item(new Item.Properties().fireResistant()));
+            ITEMS.register("characteristic/magician_characteristic", () -> new Item(new Item.Properties().fireResistant()));
     public static final DeferredItem<Item> FACELESS_CHARACTERISTIC =
-            ITEMS.register("faceless_characteristic", () -> new Item(new Item.Properties().fireResistant()));
+            ITEMS.register("characteristic/faceless_characteristic", () -> new Item(new Item.Properties().fireResistant()));
     public static final DeferredItem<Item> MARIONETTIST_CHARACTERISTIC =
-            ITEMS.register("marionettist_characteristic", () -> new Item(new Item.Properties().fireResistant()));
+            ITEMS.register("characteristic/marionettist_characteristic", () -> new Item(new Item.Properties().fireResistant()));
     public static final DeferredItem<Item> BIZARRO_SORCERER_CHARACTERISTIC =
-            ITEMS.register("bizarro_sorcerer_characteristic", () -> new Item(new Item.Properties().fireResistant()));
+            ITEMS.register("characteristic/bizarro_sorcerer_characteristic", () -> new Item(new Item.Properties().fireResistant()));
     public static final DeferredItem<Item> SCHOLAR_OF_YORE_CHARACTERISTIC =
-            ITEMS.register("scholar_of_yore_characteristic", () -> new Item(new Item.Properties().fireResistant()));
+            ITEMS.register("characteristic/scholar_of_yore_characteristic", () -> new Item(new Item.Properties().fireResistant()));
     public static final DeferredItem<Item> MIRACLE_INVOKER_CHARACTERISTIC =
-            ITEMS.register("miracle_invoker_characteristic", () -> new Item(new Item.Properties().fireResistant()));
+            ITEMS.register("characteristic/miracle_invoker_characteristic", () -> new Item(new Item.Properties().fireResistant()));
     public static final DeferredItem<Item> ATTENDANT_OF_MYSTERIES_CHARACTERISTIC =
-            ITEMS.register("attendant_of_mysteries_characteristic", () -> new Item(new Item.Properties().fireResistant()));
+            ITEMS.register("characteristic/attendant_of_mysteries_characteristic", () -> new Item(new Item.Properties().fireResistant()));
     //endregion
 
     public static void register(IEventBus eventBus) {
