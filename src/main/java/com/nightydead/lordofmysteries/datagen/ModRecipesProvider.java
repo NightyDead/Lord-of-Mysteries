668f8348e2da59e05753bb5840f994c7424abf62
shangwu <1536434622@qq.com>
2026-07-06 14:44:57 +0800

    添加四种植物和金薄荷叶配方

--- src/main/java/com/nightydead/lordofmysteries/datagen/ModRecipesProvider.java
+++ src/main/java/com/nightydead/lordofmysteries/datagen/ModRecipesProvider.java
@@ -1,9 +1,13 @@
 package com.nightydead.lordofmysteries.datagen;
 
+import com.nightydead.lordofmysteries.block.ModBlocks;
+import com.nightydead.lordofmysteries.item.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.neoforged.neoforge.common.conditions.IConditionBuilder;
 
 import java.util.concurrent.CompletableFuture;
@@ -15,6 +19,12 @@ public class ModRecipesProvider extends RecipeProvider implements IConditionBuil
 
     @Override
     protected void buildRecipes(RecipeOutput recipeOutput) {
+        // 金薄荷方块 → 2个金薄荷叶（无形状配方）
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.GOLD_MINT_LEAF.get(), 2)
+                .requires(ModBlocks.GOLD_MINT_HERB.get())
+                .unlockedBy("has_gold_mint_herb", has(ModBlocks.GOLD_MINT_HERB.get()))
+                .save(recipeOutput, "lordofmysteries:gold_mint_leaf_from_herb");
+
         super.buildRecipes(recipeOutput);
     }
 }
---
+++ src/main/java/com/nightydead/lordofmysteries/item/custom/PureWaterItem.java
@@ -0,0 +1,34 @@
+package com.nightydead.lordofmysteries.item.custom;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.PotionItem;
+import net.minecraft.world.item.TooltipFlag;
+
+import java.util.List;
+
+/**
+ * 纯水产品类 - 继承原版药水行为
+ * 拥有与原版水瓶完全一致的饮用、效果、Tooltip 机制
+ * 作为炼药锅的基础溶剂使用
+ */
+public class PureWaterItem extends PotionItem {
+
+    public PureWaterItem(Properties properties) {
+        super(properties);
+    }
+
+    public static Properties createDefaultProperties() {
+        return new Properties()
+                .stacksTo(1);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        tooltipComponents.add(Component.translatable("tooltip.lordofmysteries.pure_water.desc")
+                .withStyle(ChatFormatting.AQUA));
+        // 调用父类显示药水效果提示（纯水无效果，但保留框架以便后续扩展）
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+    }
+}
